3b5f8d8ef40a9618a6b8b067b89e8a64ea0155ff
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2024-08-12 19:49:57 +0200

    Add Multiple Localized Tool

--- Assets/Localization/Scripts/LocalizationManager.cs
+++ Assets/Localization/Scripts/LocalizationManager.cs
@@ -8,7 +8,7 @@ namespace PDC.Localization
     public class LocalizationManager : MonoBehaviour
     {
         [SerializeField] Loader _loader;
-        public static int languageID = 2;
+        public static int languageID = 0;
         private static List<string> _languages = new List<string>();
         private static Dictionary<string, string[]> _localization = new Dictionary<string, string[]>();
         public static Action OnLocalizationReady;
@@ -35,27 +35,84 @@ namespace PDC.Localization
             {
                 //Debug.Log($"Key : {key} | Text found : {text != null}");
                 if (text == null) return string.Empty;
-                return SimplifyText(text[languageID]);
+                return text[languageID];
             }
         }
 
+        private readonly static Dictionary<string, string> _simplifyDico = new()
+        {
+            { "[n]", "\n" },
+            { "[/#]", "</color>" },
+            { "[b]", "<b>" },
+            { "[/b]", "</b>" },
+            { "[i]", "<i>" },
+            { "[/i]", "</i>" },
+        };
         private static string SimplifyText(string text)
         {
             var t = text;
-            t = t.Replace("[n]", "\n");
+
+            while(t.Contains('[') && t.Contains("]"))
+            {
+                int startIndex = t.IndexOf('[');
+                int endIndex = t.IndexOf(']');
+                int length = endIndex - startIndex + 1;
+
+                var c = t.Substring(startIndex, length);
+
+                if (_simplifyDico.ContainsKey(c))
+                {
+                    t = t.Replace(c, _simplifyDico[c]);
+                    continue;
+                }
+
+                if(c.Length == 9 && c.Contains('#'))
+                {
+                    var value = c.Replace("[", string.Empty).Replace("]", string.Empty).Replace("#", string.Empty);
+                    t = t.Replace(c, $"<color=#{value}>");
+                    continue;
+                }
+
+                t = t.Replace(c, "");
+            }
+            
+            //Debug.Log(t);
             return t;
         }
 
         public static string LocalizeText(string text)
         {
             var t = text;
-            foreach (var key in _localization)
+            bool AsKey()
             {
-                if (t.Contains($"[{key.Key}]"))
+                foreach (var key in _localization)
                 {
-                    t = t.Replace($"[{key.Key}]", key.Value[languageID]);
+                    if (t.Contains($"[{key.Key}]"))
+                    {
+                        return true;
+                    }
                 }
+                return false;
             }
+
+            string ReplaceAllKey(string text)
+            {
+                var temp = t;
+                foreach (var key in _localization)
+                {
+                    if (temp.Contains($"[{key.Key}]"))
+                    {
+                        temp = temp.Replace($"[{key.Key}]", key.Value[languageID]);
+                    }
+                }
+                return temp;
+            }
+
+            while (AsKey())
+            {
+                t = ReplaceAllKey(t);
+            }
+            
             t.Replace($"\r\n", string.Empty);
             return SimplifyText(t);
         }
