d0f9c392120d070dc0d095e5d8b64b315246c302
rBLOT-Arcenz <tsuku0611@protonmail.com>
2024-09-02 18:39:46 +0200

    Add rotation to movements

--- Assets/Character/Scripts/CharacterMovements.cs
+++ Assets/Character/Scripts/CharacterMovements.cs
@@ -8,8 +8,11 @@ public class CharacterMovements : MonoBehaviour
 {
     public float speed = 100f;
     Rigidbody rb;
-    Vector3 movement;
     PlayerInputEvent PIE;
+
+    float _currentVelocity;
+    Vector2 targetStickAngle;
+    [SerializeField] float smoothRotaTime = 0.05f;
 
     void Start()
     {
@@ -18,14 +21,29 @@ public class CharacterMovements : MonoBehaviour
 
     private void Awake()
     {
-        PIE = GameObject.Find("InputManager").GetComponent<PlayerInputEvent>();
+        PIE = GameObject.Find("InputManager").GetComponent<PlayerInputEvent>(); //To change when the InputManager will be moved in the Game Manager
     }
 
     public void FixedUpdate()
     {
-        Vector2 mvVector = PIE.PlayerInputAction.Player.Movement.ReadValue<Vector2>();
-        Vector2 LerpedVelocity = Vector2.Lerp(new Vector2(rb.velocity.x, rb.velocity.z), mvVector, 1f).normalized * speed;
+        doRotation();
+        doMovement();
+    }
+
+    void doRotation()
+    {
+        if (PIE.PlayerInputAction.Player.Movement.ReadValue<Vector2>() != new Vector2(0, 0))
+        {
+            targetStickAngle = PIE.PlayerInputAction.Player.Movement.ReadValue<Vector2>();
+        }
+        var targetAngle = Mathf.Atan2(targetStickAngle.x, targetStickAngle.y) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothRotaTime);
+        transform.rotation = Quaternion.Euler(0, angle, 0);
+    }
+
+    void doMovement()
+    {
+        Vector2 LerpedVelocity = Vector2.Lerp(new Vector2(rb.velocity.x, rb.velocity.z), PIE.PlayerInputAction.Player.Movement.ReadValue<Vector2>() , 1f).normalized * speed;
         rb.velocity = Quaternion.Euler(0, 45, 0) * new Vector3(LerpedVelocity.x, 0, LerpedVelocity.y);
-        Debug.Log(LerpedVelocity);
     }
 }
