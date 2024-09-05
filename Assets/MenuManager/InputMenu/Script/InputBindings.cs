7b9e8f0af2c88386f760e67dfa45cbfbaa461080
Kyla Crea <fetuteddypro@gmail.com>
2024-09-05 19:13:05 +0200

    Séparation des Menu en fonction du controler lancé

--- Assets/MenuManager/InputMenu/Script/InputBindings.cs
+++ Assets/MenuManager/InputMenu/Script/InputBindings.cs
@@ -7,14 +7,32 @@ using UnityEngine.UI;
 public class InputBindings : MonoBehaviour
 {
     public PlayerInput playerInput;
-
     public Text InteractionText;
 
+    [SerializeField] private GameObject keyBoardScreen;
+    [SerializeField] private GameObject gamepadScreen;
+
     private PlayerInputAction playerInputAction;
+
     private void Awake()
     {
         playerInputAction = new PlayerInputAction();
         InteractionText.text = playerInputAction.Player.Interaction.GetBindingDisplayString();
+
+        Close();
+    }
+
+    public void ControllerScreen()
+    {
+        if (Gamepad.all.Count > 0) gamepadScreen.SetActive(true);
+        else 
+            keyBoardScreen.SetActive(true);
+    }
+
+    public void Close()
+    {
+        keyBoardScreen.gameObject.SetActive(false);
+        gamepadScreen.SetActive(false);
     }
 
     public void InteractionRebind()
