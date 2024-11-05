81df0c03aed46df67a005f271e7f43761fdc9bc5
Kyla Crea <fetuteddypro@gmail.com>
2024-11-05 21:10:56 +0100

    Memory SO + Memory Manager (acte de présence)

---
+++ Assets/RPDPC/MANAGER/MemoryManager/Script/MemorySO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Memory", menuName = ("Game/Memory"), order =1)]
+public class MemorySO : ScriptableObject
+{
+    public Action _action;
+    public bool _disposed = false;
+    public int relationValue;
+    public int defenceValue;
+    private List<TranslatedWord> translatedWords = new List<TranslatedWord>();
+
+    public void RunEvent()
+    {
+        _action?.Invoke();
+    }
+}
