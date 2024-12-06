920da62620d7b9af45df23ae1284ce0f6de67dbe
Sulivan-Michalon <sulivan.michalon@gmail.com>
2024-12-06 18:24:27 +0100

    Fix : navigation, on peut naviguer à la manette !

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -9,18 +10,27 @@ public class MainMenuManager : MonoBehaviour
 
     public GameObject UIOption;
     public GameObject UIMenu;
-    
+
+    public EventSystem UINavigation;
+    public GameObject btnFocusOption;
+
+    void Start()
+    {
+        UINavigation = EventSystem.current;
+    }
+
+
 
     public void playButton()
     {
         SceneManager.LoadScene(GameSceneName);
-        
     }
 
     public void OptionButton()
     {
         UIOption.SetActive(true);
         UIMenu.SetActive(false);
+        UINavigation.SetSelectedGameObject(btnFocusOption);
     }
 
     public void quitButton()
