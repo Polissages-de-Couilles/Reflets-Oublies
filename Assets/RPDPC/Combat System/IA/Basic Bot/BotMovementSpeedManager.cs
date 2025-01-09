9cff6423bbbcb106718fe512f8b2ab28742d9996
rBLOT-Arcenz <tsuku0611@protonmail.com>
2025-01-09 19:47:48 +0100

    Add animCurve speed manager

---
+++ Assets/RPDPC/Combat System/IA/Basic Bot/BotMovementSpeedManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+public class BotMovementSpeedManager : MonoBehaviour
+{
+    [SerializeField] AnimationCurve speedCurve;
+    [SerializeField] NavMeshAgent agent;
+    float timer = 0f;
+    float baseSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseSpeed = agent.speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.hasPath)
+        {
+            agent.speed = speedCurve.Evaluate(timer) * baseSpeed;
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0f;
+        }
+    }
+}
