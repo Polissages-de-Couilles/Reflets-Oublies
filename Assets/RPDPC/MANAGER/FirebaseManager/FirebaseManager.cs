0e4fef866454742cb8b5d11973a433dee990a8e1
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2025-01-21 22:29:22 +0100

    Firebase Init Fix

--- Assets/RPDPC/MANAGER/FirebaseManager/FirebaseManager.cs
+++ Assets/RPDPC/MANAGER/FirebaseManager/FirebaseManager.cs
@@ -1,21 +1,28 @@
 using Firebase;
+using Firebase.Database;
 using Proyecto26;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class FirebaseManager : MonoBehaviour
 {
-    private const string firebase_url = "https://rpdpc-a1f69-default-rtdb.europe-west1.firebasedatabase.app/";
-    private string GetURL(string url) => firebase_url + url + ".json";
+    //private const string firebase_url = "https://rpdpc-a1f69-default-rtdb.europe-west1.firebasedatabase.app/";
+    //private string GetURL(string url) => firebase_url + url + ".json";
 
+    private const string USER_KEY = "USER";
+    private FirebaseDatabase _database;
     public Action OnFirebaseInitialized;
 
     public void Start()
     {
+        OnFirebaseInitialized += OnFirebaseInit;
         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => 
         {
             if(task.Exception != null)
@@ -24,8 +31,52 @@ public class FirebaseManager : MonoBehaviour
                 return;
             }
 
+            Debug.Log("Firebase is init");
+            _database = FirebaseDatabase.DefaultInstance;
             OnFirebaseInitialized?.Invoke();
         });
+    }
+
+    private async void OnFirebaseInit()
+    {
+        await SetUserData(new("Ryan", 1, 1, 1));
+
+        var data = await GetUserData("Ryan");
+        Debug.Log(data.name);
+
+        var datas = await GetAllUserData();
+        foreach (var d in datas)
+        {
+            Debug.Log($"{d.name}");
+        }
+    }
+
+    public async Task SetUserData(UserData userData)
+    {
+        await _database.GetReference(USER_KEY).Database.GetReference(userData.name).SetRawJsonValueAsync(JsonUtility.ToJson(userData));
+    }
+
+    public async Task<UserData> GetUserData(string key)
+    {
+        var dataSnapshot = await _database.GetReference(USER_KEY).Database.GetReference(key).GetValueAsync();
+        if (!dataSnapshot.Exists)
+        {
+            return null;
+        }
+        return JsonUtility.FromJson<UserData>(dataSnapshot.GetRawJsonValue());
+    }
+
+    public async Task<List<UserData>> GetAllUserData()
+    {
+        var dataList = await _database.GetReference(USER_KEY).OrderByChild("name").GetValueAsync();
+
+        var list = new List<UserData>();
+        foreach (var data in dataList.Children.Reverse())
+        {
+            var d = JsonUtility.FromJson<UserData>(data.GetRawJsonValue());
+            list.Add(d);
+        }
+        return list;
     }
 
     //private void AddUser(string name, float x, float y, float z)
