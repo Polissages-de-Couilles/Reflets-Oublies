f9f50162e2d119f63f90e28f932a9e5704ec9c04
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2025-02-14 18:45:13 +0100

    Fix BotDeathManager

--- Assets/RPDPC/Combat System/IA/Basic Bot/BotDeathManager.cs
+++ Assets/RPDPC/Combat System/IA/Basic Bot/BotDeathManager.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(StateMachineManager))]
 public class BotDeathManager : MonoBehaviour
 {
+    const string DeathAnimName = "Die";
+    StateMachineManager stateMachine;
     public Action OnBotDied;
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<IDamageable>().OnDamageTaken += CallCheckBotHealth;
+        stateMachine = GetComponent<StateMachineManager>();
     }
 
     void CallCheckBotHealth(float damageTaken, float playerHealth)
@@ -33,16 +38,16 @@ public class BotDeathManager : MonoBehaviour
                     MSE.spawnedMobs.Remove(gameObject);
                 }
             }
-            
+
             stateMachine.enabled = false;
-            
+
             GetComponent<StateMachineManager>().enabled = false;
 
             if (TryGetComponent<MoneyDrop>(out MoneyDrop money))
             {
                 money.DropMonney();
             }
-            
+
             OnBotDied?.Invoke();
             stateMachine.Animator.Play(DeathAnimName);
             float animationDuration = stateMachine.Animator.runtimeAnimatorController.animationClips.ToList().Find(x => x.name == DeathAnimName).length;
