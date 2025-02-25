0cbf2df2823e4ec833f1b98d757ad7f63533a38c
rBLOT-Arcenz <r.blot@protonmail.com>
2025-02-25 15:55:11 +0100

    Small fix de TurnToPlayer

--- Assets/RPDPC/Combat System/IA/Conditions/Basic Conditions/PlayerIsVisible.cs
+++ Assets/RPDPC/Combat System/IA/Conditions/Basic Conditions/PlayerIsVisible.cs
@@ -9,7 +9,7 @@ public class PlayerIsVisible : ConditionBase
     GameObject parent;
     GameObject player;
     CharacterController playerCollider;
-    CapsuleCollider parentCollider;
+    Collider parentCollider;
     List<Vector3> directions;
     StateManager sm;
     [SerializeField] bool fulfillIfTalking;
@@ -20,7 +20,7 @@ public class PlayerIsVisible : ConditionBase
         this.player = player;
         playerCollider = player.GetComponent<CharacterController>();
         sm = this.player.GetComponent<StateManager>();
-        parentCollider = parent.GetComponent<CapsuleCollider>();
+        parentCollider = parent.GetComponent<Collider>();
     }
 
     public override bool isConditionFulfilled()
@@ -31,11 +31,11 @@ public class PlayerIsVisible : ConditionBase
         }
 
         directions = new List<Vector3> {
-            player.transform.position,
-            player.transform.position + new Vector3(0, playerCollider.height / 2, 0),
-            player.transform.position + new Vector3(0, -playerCollider.height / 2, 0),
-            player.transform.position + new Vector3(playerCollider.radius, 0, 0),
-            player.transform.position + new Vector3(-playerCollider.radius, 0, 0),
+            playerCollider.bounds.center,
+            playerCollider.bounds.center + new Vector3(0, playerCollider.height / 2 - 0.1f, 0),
+            playerCollider.bounds.center + new Vector3(0, -playerCollider.height / 2 + 0.1f, 0),
+            playerCollider.bounds.center + new Vector3(playerCollider.radius- 0.1f, 0, 0),
+            playerCollider.bounds.center + new Vector3(-playerCollider.radius + 0.1f, 0, 0),
             //player.transform.position + new Vector3(-playerCollider.radius/ 2, playerCollider.height / 4, 0),          Points formant un losange, que j'ai jugés inutiles, mais je les laisse au cas où en fait ça l'est
             //player.transform.position + new Vector3(-playerCollider.radius/ 2, -playerCollider.height / 4, 0),
             //player.transform.position + new Vector3(playerCollider.radius / 2, playerCollider.height / 4, 0),
@@ -56,13 +56,13 @@ public class PlayerIsVisible : ConditionBase
         float RayHeight = 0f;
         if (parentCollider != null) 
         {
-            RayHeight = parentCollider.height / 2;
+            RayHeight = parentCollider.bounds.size.y / 2;
         }
-        if (Physics.Raycast(parent.transform.position + new Vector3(0, RayHeight, 0), -(parent.transform.position - direction), out hit, Mathf.Infinity))
+        Vector3 origin = parent.transform.position + new Vector3(0, RayHeight, 0);
+        if (Physics.Raycast(origin, -(origin - direction), out hit, Mathf.Infinity))
         {
-            Debug.DrawLine(parent.transform.position, hit.point);
-            //Debug.Log(Vector3.Angle(parent.transform.forward.normalized, -(parent.transform.position - direction).normalized));
-            if (hit.collider.gameObject == player && Vector3.Angle(parent.transform.forward.normalized, -(parent.transform.position - direction).normalized) <= viewAngle/2)
+            Debug.DrawLine(origin, hit.point);
+            if (hit.collider.gameObject == player && Vector3.Angle(parent.transform.forward.normalized, new Vector3(-(origin - direction).x, 0, -(origin - direction).z).normalized) <= viewAngle/2)
             {
                 return true;
             }
--- Assets/RPDPC/Combat System/IA/States/Basics States/Entities/TurnToPlayerEntity.cs
+++ Assets/RPDPC/Combat System/IA/States/Basics States/Entities/TurnToPlayerEntity.cs
@@ -35,14 +35,33 @@ public class TurnToPlayerEntity : StateEntityBase
     protected IEnumerator DoLookAt()
     {
         manager.shouldSearchStates = false;
-        
-        while (Quaternion.Angle(Quaternion.Euler(0, parent.transform.eulerAngles.y, 0), Quaternion.LookRotation(player.transform.position - parent.transform.position, Vector3.up)) > 5)
+
+        float angle = 5f;
+        if (Vector3.Distance(parent.transform.position, player.transform.position) < 11.5f)
+        {
+            angle = 5f + 250f / (11.5f + Vector3.Distance(parent.transform.position, player.transform.position));
+        }
+
+        while (Quaternion.Angle(Quaternion.Euler(0, parent.transform.eulerAngles.y, 0), Quaternion.LookRotation(player.transform.position - parent.transform.position, Vector3.up)) > angle)
         {
             Debug.Log(Quaternion.Angle(Quaternion.Euler(0, parent.transform.eulerAngles.y, 0), Quaternion.LookRotation(player.transform.position - parent.transform.position, Vector3.up)));
+            Debug.Log(angle);
+            // ~11.5
+
+            if (Vector3.Distance(parent.transform.position, player.transform.position) < 11.5f)
+            {
+                angle = 5f + 250f / (11.5f + Vector3.Distance(parent.transform.position, player.transform.position));
+            }
+            else
+            {
+                angle = 5;
+            }
+
             Vector3 targetVector = new Vector3(player.transform.position.x, parent.transform.position.y, player.transform.position.z);
             parent.transform.DOLookAt(targetVector, turnDuration * (Quaternion.Angle(Quaternion.Euler(0, parent.transform.eulerAngles.y, 0), Quaternion.LookRotation(player.transform.position - parent.transform.position, Vector3.up)) / 360f)).SetEase(Ease.Linear);
             yield return new WaitForFixedUpdate();
         }
+        yield return new WaitForFixedUpdate();
         manager.shouldSearchStates = true;
         ExitState();
         
