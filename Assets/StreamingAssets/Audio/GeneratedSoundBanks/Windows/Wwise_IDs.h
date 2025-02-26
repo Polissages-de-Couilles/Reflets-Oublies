1a5bff3c222db5d92c4344f4bef185953750332e
Sulivan-Michalon <sulivan.michalon@gmail.com>
2025-02-26 11:25:50 +0100

    suicide saucial mais sans la game scene

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Windows/Wwise_IDs.h
@@ -0,0 +1,117 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID COMBATMUSIC = 3733692670U;
+        static const AkUniqueID COMBATMUSICPAUSE = 3640620548U;
+        static const AkUniqueID COMBATMUSICRESUME = 4123088407U;
+        static const AkUniqueID FORETMUSIC = 2264695890U;
+        static const AkUniqueID FORETMUSICPAUSE = 2716092448U;
+        static const AkUniqueID FORETMUSICRESUME = 1829540987U;
+        static const AkUniqueID MARIOMUSIC = 3428957736U;
+        static const AkUniqueID MARIOMUSICPAUSE = 3360282654U;
+        static const AkUniqueID MARIOMUSICRESUME = 797962905U;
+        static const AkUniqueID PLAY_ASHLEY_COUGH = 883066387U;
+        static const AkUniqueID PLAY_ASHLEY_CRY = 2581246001U;
+        static const AkUniqueID PLAY_ASHLEY_HEINN = 4130244703U;
+        static const AkUniqueID PLAY_ASHLEY_HMM = 2831483387U;
+        static const AkUniqueID PLAY_ASHLEY_LAUGH = 1946362930U;
+        static const AkUniqueID PLAY_ASHLEY_WOUH = 2137976346U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_JACK_COUGH = 968975072U;
+        static const AkUniqueID PLAY_JACK_CRY = 1403254254U;
+        static const AkUniqueID PLAY_JACK_HEINN = 3395388088U;
+        static const AkUniqueID PLAY_JACK_HMM = 951685384U;
+        static const AkUniqueID PLAY_JACK_LAUGH = 3728340277U;
+        static const AkUniqueID PLAY_JACK_WOUH = 2754582667U;
+        static const AkUniqueID PLAY_LASERARME = 2320881544U;
+        static const AkUniqueID PLAY_MICKEY_MOUSE_SOUND_EFFECT = 2501766892U;
+        static const AkUniqueID RAPH_DIALOGUE_1_1 = 3170927391U;
+        static const AkUniqueID RAPH_DIALOGUE_1_2 = 3170927388U;
+        static const AkUniqueID SMASHMUSIC = 572284388U;
+        static const AkUniqueID SMASHMUSICPAUSE = 3395474434U;
+        static const AkUniqueID SMASHMUSICRESUME = 750352341U;
+        static const AkUniqueID VILLAGEMUSIC = 3625871072U;
+        static const AkUniqueID VILLAGEMUSICPAUSE = 2606775814U;
+        static const AkUniqueID VILLAGEMUSICRESUME = 1037476801U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace GUNSWITCH
+        {
+            static const AkUniqueID GROUP = 119732153U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GUN1 = 688956122U;
+                static const AkUniqueID GUN2 = 688956121U;
+            } // namespace SWITCH
+        } // namespace GUNSWITCH
+
+        namespace ISLASER
+        {
+            static const AkUniqueID GROUP = 773204178U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LASER = 3982605422U;
+                static const AkUniqueID NOTLASER = 2734451473U;
+            } // namespace SWITCH
+        } // namespace ISLASER
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID COMBATMUSIC = 3733692670U;
+        static const AkUniqueID FORETMUSIC = 2264695890U;
+        static const AkUniqueID MARIOMUSIC = 3428957736U;
+        static const AkUniqueID MASTERVOLUME = 2918011349U;
+        static const AkUniqueID MUSICVOLUME = 2346531308U;
+        static const AkUniqueID SMASHMUSIC = 572284388U;
+        static const AkUniqueID TRANSITIONPARAMETER = 2985490821U;
+        static const AkUniqueID VFXVOLUME = 698515981U;
+        static const AkUniqueID VILLAGEMUSIC = 3625871072U;
+        static const AkUniqueID VOICEVOLUME = 414646191U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANKPROJECT = 2838261015U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID MUSICCOMBAT = 3634348186U;
+        static const AkUniqueID MUSICFORET = 1935794824U;
+        static const AkUniqueID MUSICMARIO = 1429868846U;
+        static const AkUniqueID MUSICSMASH = 1441840878U;
+        static const AkUniqueID MUSICVILLAGE = 1844245490U;
+        static const AkUniqueID VFX = 694692527U;
+        static const AkUniqueID VOICE = 3170124113U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
