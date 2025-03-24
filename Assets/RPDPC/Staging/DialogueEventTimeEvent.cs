058ac35ef5cd6dcdec03b61b5362e5fbc1089157
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2025-03-24 18:48:40 +0100

    Wait Event

--- Assets/RPDPC/Staging/DialogueEventTimeEvent.cs
+++ Assets/RPDPC/Staging/DialogueEventTimeEvent.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 using MeetAndTalk.Event;
 using System.Linq;
 
-public class DialogueEventTimeEvent : DialogueEventSO
+public abstract class DialogueEventTimeEvent : DialogueEventSO
 {
     public bool IsEventEnd { get; set; } = false;
-
-    public override void RunEvent()
-    {
-
-    }
 
     protected virtual void EventEnd()
     {
