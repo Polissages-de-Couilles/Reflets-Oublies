5cfe552a426665f45efd0d9d3b9b86e654a8e79f
rBLOT-Arcenz <tsuku0611@protonmail.com>
2025-05-21 00:28:14 +0200

    wip : miroirs cools

--- Assets/RPDPC/Combat System/Boss/Ombre/ProjectileExplosiveMirror.cs
+++ Assets/RPDPC/Combat System/Boss/Ombre/ProjectileExplosiveMirror.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class ProjectileExplosiveMirror : ProjectileBase
@@ -7,6 +8,8 @@ public class ProjectileExplosiveMirror : ProjectileBase
     [SerializeField] Vector3 centerPos;
     [SerializeField] float spawnRadius;
     [SerializeField] float delayBeforeExplosion;
+    [SerializeField] GameObject enterexitSFX;
+    [SerializeField] float groundLevel;
     bool isDestroyed = false;
 
     protected override void LaunchProjectile()
@@ -18,11 +21,20 @@ public class ProjectileExplosiveMirror : ProjectileBase
         Quaternion LookAtRotation = Quaternion.LookRotation(GameManager.Instance.Player.transform.position - transform.position, transform.up);
         LookAtRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, LookAtRotation.eulerAngles.y, transform.rotation.eulerAngles.z);
         transform.rotation = LookAtRotation;
+        
 
         gameObject.layer = 9;
         attackCollider.Init(manager.damageDetail.doesStun, manager.damageDetail.stunDuration, manager.damageDetail.doesKnockback, manager.damageDetail.knockbackForce, KnockbackMode.MoveAwayFromAttackCollision, true, manager.launcher);
         attackCollider.OnDamageableEnterTrigger += TriggerEnter;
+        StartCoroutine(EnterTransition());
         StartCoroutine(DelayExplosion());
+    }
+
+    IEnumerator EnterTransition()
+    {
+        GameObject trans = Instantiate(enterexitSFX, new Vector3(transform.position.x, groundLevel, transform.position.z), Quaternion.Euler(new Vector3(-90, 0, 0)));
+        yield return transform.DOMove(transform.position + new Vector3(0, 4.3f, 0), 1).WaitForCompletion();
+        Destroy(trans);
     }
 
     IEnumerator DelayExplosion()
@@ -32,7 +44,9 @@ public class ProjectileExplosiveMirror : ProjectileBase
         yield return new WaitForSeconds(0.5f);
         GetComponent<Collider>().enabled = false;
         isDestroyed = true;
-        yield return new WaitForSeconds(4.5f);
+        GameObject trans = Instantiate(enterexitSFX, new Vector3(transform.position.x, groundLevel, transform.position.z), Quaternion.Euler(new Vector3(-90, 0, 0)));
+        yield return transform.DOMove(transform.position - new Vector3(0, 4.3f, 0), 1).WaitForCompletion();
+        Destroy(trans);
         Destroy(gameObject);
     }
 
