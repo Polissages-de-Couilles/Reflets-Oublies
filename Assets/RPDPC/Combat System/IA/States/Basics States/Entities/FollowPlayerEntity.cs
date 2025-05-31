8c7cc60f4d9fc76cf6e37c1f11e7da08566feb4a
rBLOT-Arcenz <tsuku0611@protonmail.com>
2025-05-31 18:34:29 +0200

    pour toi Teddy

--- Assets/RPDPC/Combat System/IA/States/Basics States/Entities/FollowPlayerEntity.cs
+++ Assets/RPDPC/Combat System/IA/States/Basics States/Entities/FollowPlayerEntity.cs
@@ -7,10 +7,14 @@ public class FollowPlayerEntity : StateEntityBase
     protected NavMeshAgent agent;
     protected bool isIntelligent;
     protected Vector3 lastKnownPos;
+    protected bool shouldStopWhenNear;
+    protected float stopDistance;
 
-    public override void Init(bool isIntelligent)
+    public override void Init(bool isIntelligent, bool shouldStopWhenNear, float stopDistance)
     {
         this.isIntelligent = isIntelligent;
+        this.shouldStopWhenNear = shouldStopWhenNear;
+        this.stopDistance = stopDistance;
     }
 
     public override void ExitState()
@@ -50,6 +54,14 @@ public class FollowPlayerEntity : StateEntityBase
             else
             {
                 manager.shouldSearchStates = true;
+            }
+        }
+
+        if (shouldStopWhenNear)
+        {
+            if (Vector3.Distance(parent.transform.position, player.transform.position) < stopDistance)
+            {
+                ExitState();
             }
         }
         
