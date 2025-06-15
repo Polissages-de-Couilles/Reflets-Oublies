27aa4e2283da329ec4ed062244452e1ef8e2bad1
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2025-06-15 22:22:47 +0200

    Sage Parano act 3

--- Assets/RPDPC/Staging/Events/Scripts/MoveToSameValue.cs
+++ Assets/RPDPC/Staging/Events/Scripts/MoveToSameValue.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MoveToSameValue : StagingEvent
 {
     [SerializeField] Transform objectToMove;
+    [SerializeField] bool useNavMesh = false;
     //[SerializeField] float duration = 1f;
 
     public override void PlayEvent()
@@ -17,7 +19,19 @@ public class MoveToSameValue : StagingEvent
             return;
         }
 
-        objectToMove.position = this.transform.position;
+        if(useNavMesh)
+        {
+            NavMeshHit myNavHit;
+            if (NavMesh.SamplePosition(transform.position, out myNavHit, 100, -1))
+            {
+                objectToMove.transform.position = myNavHit.position;
+            }
+        }
+        else
+        {
+            objectToMove.position = this.transform.position;
+        }
+
         OnEventFinished?.Invoke();
     }
 }
