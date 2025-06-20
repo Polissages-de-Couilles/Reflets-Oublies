83840322224646ff7e579dd63c0817393155713f
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2025-06-20 18:42:45 +0200

    Ombre Mise en scene

--- Assets/RPDPC/Staging/Events/Scripts/PlayAnimatorState.cs
+++ Assets/RPDPC/Staging/Events/Scripts/PlayAnimatorState.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayAnimation : StagingEvent
 {
     [SerializeField] Animator animator;
     [SerializeField] string stateName;
+    [SerializeField] bool waitAnim = false;
 
     public override void PlayEvent()
     {
@@ -16,6 +18,20 @@ public class PlayAnimation : StagingEvent
             return;
         }
         animator.Play(stateName);
+        if(waitAnim)
+        {
+            StartCoroutine(WaitAnim());
+        }
+        else
+        {
+            OnEventFinished?.Invoke();
+        }
+    }
+
+    IEnumerator WaitAnim()
+    {
+        var time = animator.runtimeAnimatorController.animationClips.ToList().Find(x => x.name == stateName).length;
+        yield return new WaitForSeconds(time);
         OnEventFinished?.Invoke();
     }
 }
